2071e40457bfec7d4af54f5bbd4eed14bee5b5e3
sudip <sudeeplamichhane18@gmail.com>
2025-10-27 12:55:05 +0545

    added public decorator inside wsjwtauthguard

--- apps/api/src/auth/guards/ws-jwt-auth.guard.ts
+++ apps/api/src/auth/guards/ws-jwt-auth.guard.ts
@@ -1,28 +1,47 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { WsException } from '@nestjs/websockets';
 // import { Observable } from 'rxjs';
 import { Socket } from 'socket.io';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class WsJwtGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if(isPublic) {
+      return true;
+    }
+    
     const client: Socket = context.switchToWs().getClient<Socket>();
 
     const token = this.extractTokenFromSocket(client);
 
     if (!token) {
-        throw new WsException('Unauthorized');
-    //   return false;
+      throw new WsException('Unauthorized');
+      //   return false;
     }
 
-    const payload = await this.jwtService.verifyAsync(token);
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
 
-    client.data.user = payload;
+      client.data.user = payload;
 
-    return true;
+      return true;
+    } catch (error) {
+      throw new WsException("Unauthorized");
+    }
   }
 
   private extractTokenFromSocket(client: Socket): string | undefined {
